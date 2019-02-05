b490421b5dc965d9dab48d09544afcda881215b8
Zhaslan <43112732+Lebra21@users.noreply.github.com>
2019-02-05 10:33:39 +0500

    Add files via upload

---
+++ hm2/cart.php
@@ -0,0 +1,183 @@
+<?php
+session_start();
+$product_ids = array();
+// Check if Add to Cart Button has been submitted
+if (filter_input(INPUT_POST, 'add_to_cart')) {
+    if (isset($_SESSION['shopping_cart'])) {
+        // keep track that how many products are in shopping cart
+        $count = count($_SESSION['shopping_cart']);
+        // create squantial array fro matching array keys to product id's
+        $product_ids = array_column($_SESSION['shopping_cart'], 'id');
+        if (!in_array(filter_input(INPUT_GET, 'id'), $product_ids)) {
+            $_SESSION['shopping_cart'][$count] =  array
+            (
+                'id' => filter_input(INPUT_GET, 'id'),
+                'name' => filter_input(INPUT_POST, 'name'),
+                'price' => filter_input(INPUT_POST, 'price'),
+                'quantity' => filter_input(INPUT_POST, 'quantity')
+            ); 
+        } else { //product already exist, increase quantity
+            // match array key to id of the product being added to the cart
+            for ($i=0; $i < count($product_ids); $i++) { 
+                if ($product_ids[$i] == filter_input(INPUT_GET, 'id')) {
+                    // add items quanity to the existing product in the array
+                    $_SESSION['shopping_cart'][$i]['quantity'] += filter_input(INPUT_POST, 'quantity');
+                }
+            }
+        }
+    }
+    else {  //if shopping cart does't exist, create first product with array key 0
+        //create array using submitted form data, start from key 0 and fill it with values
+        $_SESSION['shopping_cart'][0] =  array
+        (
+            'id' => filter_input(INPUT_GET, 'id'),
+            'name' => filter_input(INPUT_POST, 'name'),
+            'price' => filter_input(INPUT_POST, 'price'),
+            'quantity' => filter_input(INPUT_POST, 'quantity')
+        );
+    }
+}
+if (filter_input(INPUT_GET, 'action') == 'delete') {
+        // loop through all product in shopping cart until it matches with get Id variable
+        foreach($_SESSION['shopping_cart'] as $key => $product){
+            if ($product['id'] == filter_input(INPUT_GET,'id')) {
+                // remove product form the shopping cart when it mactches with the GET id
+                unset($_SESSION['shopping_cart'][$key]);
+            }
+        }
+        // reset session array keys so they match with $product_ids numeric array
+        $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
+    }
+    if(isset($_POST['check'])){
+	    $to = '170107031@stu.sdu.edu.kz';
+	    $subject = 'ShopMarket';
+
+	    $message = '
+			Thanks for your purchase
+			  
+		';
+		
+		mail($to, $subject, $message);
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <style>
+    	.products {
+		    border: 1px solid #333;
+		    background: #f1f1f1;
+		    border-radius: 5px;
+		    padding: 16px;
+		    margin-bottom: 20px;
+		}
+		.btn-lg {
+		 width: 100%;
+		}
+    </style>
+    <title>Cart</title>
+</head>
+<body>
+<div class="container">
+            <?php
+            $connect = mysqli_connect('localhost', 'root', '', 'cart');
+            $query = "SELECT * FROM `products`";
+            $stmt = mysqli_prepare($connect, $query);
+            if($stmt):
+                mysqli_stmt_bind_result($stmt, $id, $name, $image, $price);
+                mysqli_stmt_execute($stmt);
+                while(mysqli_stmt_fetch($stmt)):
+                    ?>
+                    <div class="col-sm-4 col-md-3">
+                        <form method="post" action="cart.php?action=add&id=<?php echo $id; ?>">
+                            <div class="products">
+                                <img src="<?php echo $image; ?>" class="img-responsive" alt="">
+                                <h4 class="text-info"><?php echo $name; ?></h4>
+                                <h4>$ <?php echo $price; ?></h4>
+                                <input type="text" name="quantity" class="form-control" value="1">
+                                <input type="hidden" name="name" value="<?php echo $name; ?>">
+                                <input type="hidden" name="price" value="<?php echo $price; ?>">
+                                <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-info" value="Add to Cart">
+
+                            </div>
+                        </form>
+                    </div>
+                    <?php
+                endwhile;
+            endif;
+            ?>
+            <div style="clear:both"></div>
+
+            <div class="table-responsive">
+                <table class="table">
+                    <tr><th colspan="5"><h3>Order Details</h3></th></tr>
+                    <tr>
+                        <th width="40%">Product Name</th>
+                        <th width="10%">Quantity</th>
+                        <th width="20%">Price</th>
+                        <th width="15%">Total</th>
+                        <th width="5%">Action</th>
+                    </tr>
+
+                    <?php 
+                        if (!empty($_SESSION['shopping_cart'])):
+                            $total = 0;
+                            foreach ($_SESSION['shopping_cart'] as $key => $product):
+                                ?>
+                                <tr>
+                                    <td><?php echo $product['name']; ?></td>
+                                    <td><?php echo $product['quantity']; ?></td>
+                                    <td>$<?php echo $product['price']; ?></td>
+                                    <td>$<?php echo number_format($product['quantity'] * $product['price'], 2); ?></td>
+                                    <td>
+                                        <a href="cart.php?action=delete&id=<?php echo $product['id']; ?>">
+                                            <div class="btn-danger">Remove</div>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php
+                                $total = $total + ($product['quantity'] * $product['price']);
+                            endforeach;
+                            ?>
+                            <tr>
+                                <td colspan="3" align="right">Total</td>
+                                <td align="right">$ <?php echo number_format($total, 2); ?></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="5">
+                                    <?php 
+                                        if (isset($_SESSION['shopping_cart'])):
+                                            if (count($_SESSION['shopping_cart']) > 0 ):
+                                    ?>
+                                            
+                                    <?php
+                                            endif;
+                                        endif;
+                                    ?>
+                                </td>
+                            
+                            </tr>
+
+                            <?php
+                        endif;
+                    ?>
+                   
+
+
+                </table>
+            </div>
+</div>
+<form method="POST">
+	 <input type="submit" name="check" id="a" value="Check">
+</form>
+
+</body>
+</html>
